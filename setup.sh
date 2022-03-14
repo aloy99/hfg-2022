dd31531e4abb3df7933c009d5efe1f1f170589cc
aloy99 <aloysiuschowhc@gmail.com>
2022-03-14 16:36:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-echo '
-[theme]
-primaryColor="#8a547b"
-backgroundColor="#F4CEEA"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#543478"
-font="sans serif"
-[server]
-headless = true
-port = $PORT
-enableCORS = false
-' > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
