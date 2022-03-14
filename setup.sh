f390783f9a7933834770fbdcea4e9bdddfea8b24
aloy99 <aloysiuschowhc@gmail.com>
2022-03-14 16:40:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]
+primaryColor="\#8a547b"\n\
+backgroundColor="\#F4CEEA"\n\
+secondaryBackgroundColor="\#F0F2F6"\n\
+textColor="\#543478"\n\
+font="sans serif"\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
